22df00fcfb15baa8a613ca85c85a4bfd7224c042
HarisAvdicevic <haris.avdi93@gmail.com>
2021-11-15 18:36:15 +0100

    Cleared task #7

--- app.js
+++ app.js
@@ -92,9 +92,9 @@ const eatable = [];
 const trash = [];
 for (let i = 0; i < fruits.length; i++) {
     if (fruits[i] === "apelsin" || fruits[i] === "päron") {
-        trash.push(fruits[i]);
+        trash.push(" " + fruits[i]);
     } else {
-        eatable.push(fruits[i]);
+        eatable.push(" " + fruits[i]);
     }
 }
 
@@ -121,10 +121,10 @@ Ogift: Carl,Karin
 */
 
 const persons = [
-    { name: " Christian", age: 31, married: true },
-    { name: " Diana", age: 29, married: true },
-    { name: " Carl", age: 28, married: false },
-    { name: " Karin", age: 29, married: false },
+    { name: "Christian", age: 31, married: true },
+    { name: "Diana", age: 29, married: true },
+    { name: "Carl", age: 28, married: false },
+    { name: "Karin", age: 29, married: false },
 ];
 
 const married = [];
@@ -134,19 +134,19 @@ const underThirty = [];
 
 for (let key in persons) {
     if (persons[key].age >= 30) {
-        overThirty.push(persons[key].name);
+        overThirty.push(" " + persons[key].name);
     } else {
-        underThirty.push(persons[key].name);
+        underThirty.push(" " + persons[key].name);
     }
     if (persons[key].married === true) {
-        married.push(persons[key].name);
+        married.push(" " + persons[key].name);
     } else {
-        notMarried.push(persons[key].name);
+        notMarried.push(" " + persons[key].name);
     }
 }
 
 function taskSix() {
-    document.getElementById("answer-six").innerHTML = "Över 30: " + overThirty + "<br>" + "Under 30: " + underThirty + "<br>" + "Gift: " + married + "<br>" + "Ogift: " + notMarried;
+    document.getElementById("answer-six").innerHTML = "<b>Över 30: " + overThirty + "<br>" + "Under 30: " + underThirty + "<br>" + "Gift: " + married + "<br>" + "Ogift: " + notMarried + "</b>";
 }
 
 /*
@@ -155,11 +155,10 @@ Dela upp strängen addMeSomeLineBreaks vid varje komma och skriv
 ut texten på en ny rad i diven "answer-seven"
 */
 
-const addMeSomeLineBreaks =
-    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+const addMeSomeLineBreaks = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
 
 function taskSeven() {
-    alert("Replace this alert with a solution");
+    document.getElementById("answer-seven").innerHTML = addMeSomeLineBreaks.replace(/,/g, "<br>");
 }
 
 /*
